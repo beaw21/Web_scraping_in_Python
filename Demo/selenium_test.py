0457cbafe9f8e47704f680c1e53c4832321f1b10
bill patcha <beawglom2013@gmail.com>
2022-07-12 14:23:09 +0700

    add selenium

--- Demo/selenium_test.py
+++ Demo/selenium_test.py
@@ -1,10 +1,9 @@
+import sys
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+#check version
+print(sys.path)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://camt.cmu.ac.th")
 
-driver = webdriver.Chrome()
-driver.get("https://github.com/apache/hive/pulls?q=is%3Apr+is%3Aclosed")
-
-elements = driver.find_element(by="d-block d-md-none position-absolute top-0 bottom-0 left-0 right-0")
-
-print(elements)
