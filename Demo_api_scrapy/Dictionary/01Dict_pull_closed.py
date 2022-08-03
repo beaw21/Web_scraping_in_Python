b95b3765ff0b854e677f175d9eeb2a2e52c0767e
bill patcha <beawglom2013@gmail.com>
2022-08-03 16:03:39 +0700

    created class scrapy spider

--- Demo_api_scrapy/Dictionary/01Dict_pull_closed.py
+++ Demo_api_scrapy/Dictionary/01Dict_pull_closed.py
@@ -1,12 +1,16 @@
 import json
 import importlib
-import page_one
 
 from datetime import timedelta
 import pandas as pd
 import numpy as np
 import requests
 import io
+
+import scrapy
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
 
 pull_closed = requests.get('https://api.github.com/repos/apache/hive/pulls?state=closed')
 data_json_pull_closed = pull_closed.json()
@@ -27,3 +31,16 @@ myDict["pulls"] = new_df['url_pull']
 
 Dict = pd.DataFrame.from_dict(new_df)
 print(Dict)
+
+
+class ErrbackSpider(scrapy.Spider):
+    name = "Pang pulls"
+    start_urls_pulls = Dict['url_pull']
+
+    def start_requests(self):
+        for u in self.start_urls_pulls:
+            yield scrapy.Request(u, callback=self.parse_httpbin, errback=self.errback_httpbin, dont_filter=True)
+
+    def parse_httpbin(self, response):
+        self.logger.info('Got successful response from {}'.format(response.url))
+        print(response)
