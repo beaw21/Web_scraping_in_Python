5ba79bfea80deb6f0c491694b81f9455f79b54a0
bill patcha <beawglom2013@gmail.com>
2022-08-10 16:26:40 +0700

    set by set get objects in each URl

--- Demo_api_scrapy/Dictionary/01Dict_pull_closed.py
+++ Demo_api_scrapy/Dictionary/01Dict_pull_closed.py
@@ -1,40 +1,47 @@
-import json
-import importlib
-
-from datetime import timedelta
 import pandas as pd
 import numpy as np
 import requests
-import io
-
-import scrapy
-from scrapy.spidermiddlewares.httperror import HttpError
-from twisted.internet.error import DNSLookupError
-from twisted.internet.error import TimeoutError, TCPTimedOutError
 
 pull_closed = requests.get('https://api.github.com/repos/apache/hive/pulls?state=closed')
 data_json_pull_closed = pull_closed.json()
 
-length = list(data_json_pull_closed)
+df = pd.DataFrame(data_json_pull_closed)
 
-df = pd.DataFrame(length)
-
+# mew data Frame
 new_data = []
 
 new_df = pd.DataFrame(new_data)
 new_df['ID'] = df['id'].values
 new_df['url_pull'] = df['url'].values
 
-myDict = {}
-myDict["ID_pulls"] = new_df['ID']
-myDict["pulls"] = new_df['url_pull']
-
+# Creates DataFrame object from dictionary by columns or by index allowing dtype specification
 Dict = pd.DataFrame.from_dict(new_df)
-print(Dict)
 
-get_requests_url = requests.get(Dict['url_pull']).json()
-print(get_requests_url)
+# Creates a Value for call in each Url
+get_url = Dict.apply(lambda x: x["url_pull"], 1)
 
-set_index = Dict.set_index("ID").to_dict()["url_pull"]
+# get obj json in each URL
+for i in get_url:
+    requests.get(i).json()
+    print(requests.get(i).json())
+    if i == 29:
+        break;
 
-Dict.apply(lambda x: x["url_pull"], 1)
+# get_obj = requests.get(get_url[0]).json()
+
+
+# create an empty pandas to dictionary
+# dct = pd.DataFrame.to_dict(new_df)
+
+# set index by ID in Dict
+# set_index = Dict.set_index("ID").to_dict()["url_pull"]
+
+# Creates a Value for call in each Url
+# dict_values = set_index.values()
+
+
+# getUrl = Dict.apply(lambda x: x["url_pull"], 1)
+# triedGetUrl = requests.get(Dict['url_pull'][0]).json()
+
+#
+# data_json = requests.get(getUrl[0]).json()
