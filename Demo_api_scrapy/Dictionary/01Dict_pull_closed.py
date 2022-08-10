ed794e27a220a4e54d8ef9cfdacc920542752166
bill patcha <beawglom2013@gmail.com>
2022-08-11 02:48:00 +0700

    git  data and Creates DataFrame for column url pull

--- Demo_api_scrapy/Dictionary/01Dict_pull_closed.py
+++ Demo_api_scrapy/Dictionary/01Dict_pull_closed.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import requests
 
 pull_closed = requests.get('https://api.github.com/repos/apache/hive/pulls?state=closed')
@@ -7,7 +6,7 @@ data_json_pull_closed = pull_closed.json()
 
 df = pd.DataFrame(data_json_pull_closed)
 
-# mew data Frame
+# mew data Frame for pull state closed
 new_data = []
 
 new_df = pd.DataFrame(new_data)
@@ -17,31 +16,24 @@ new_df['url_pull'] = df['url'].values
 # Creates DataFrame object from dictionary by columns or by index allowing dtype specification
 Dict = pd.DataFrame.from_dict(new_df)
 
+# create an empty pandas to dictionary
+dct = pd.DataFrame.to_dict(new_df)
+
 # Creates a Value for call in each Url
 get_url = Dict.apply(lambda x: x["url_pull"], 1)
 
 # get obj json in each URL
-for i in get_url:
-    requests.get(i).json()
-    print(requests.get(i).json())
-    if i == 29:
-        break;
+get_obj = get_url.apply(lambda x: requests.get(x).json())
 
-# get_obj = requests.get(get_url[0]).json()
-
-
-# create an empty pandas to dictionary
-# dct = pd.DataFrame.to_dict(new_df)
-
-# set index by ID in Dict
-# set_index = Dict.set_index("ID").to_dict()["url_pull"]
-
-# Creates a Value for call in each Url
-# dict_values = set_index.values()
-
-
-# getUrl = Dict.apply(lambda x: x["url_pull"], 1)
-# triedGetUrl = requests.get(Dict['url_pull'][0]).json()
-
-#
-# data_json = requests.get(getUrl[0]).json()
+# get data and Creates DataFrame
+df_dict = pd.DataFrame({"id_pull": get_obj.apply(lambda x: x['id']),
+                        "user_id": get_obj.apply(lambda x: x['user']['id']),
+                        "open_time": get_obj.apply(lambda x: x['created_at']),
+                        "closed_time": get_obj.apply(lambda x: x['closed_at']),
+                        "amount_commit": get_obj.apply(lambda x: x['commits']),
+                        "amount_review": get_obj.apply(lambda x: x['review_comments']),
+                        "amount_addition": get_obj.apply(lambda x: x['additions']),
+                        "amount_deletion": get_obj.apply(lambda x: x['deletions']),
+                        "amount_changed_files": get_obj.apply(lambda x: x['changed_files']),
+                        "number_sha": get_obj.apply(lambda x: x['base']['sha'])
+                        })
